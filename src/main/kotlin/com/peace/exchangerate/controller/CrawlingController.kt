19bf6e14ba0558f59dc9620917acbd235711f088
PyeongGangKim <21500185@handong.edu>
2023-07-30 23:10:22 +0900

    feat: crawling service 추가

--- src/main/kotlin/com/peace/exchangerate/controller/CrawlingController.kt
+++ src/main/kotlin/com/peace/exchangerate/controller/CrawlingController.kt
@@ -1,9 +1,11 @@
 package com.peace.exchangerate.controller
 
+import com.peace.exchangerate.service.CrawlingService
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class CrawlingController {
-
+class CrawlingController(
+    private val crawlingService: CrawlingService,
+) {
 
 }
---
+++ src/main/kotlin/com/peace/exchangerate/service/CrawlingService.kt
@@ -0,0 +1,9 @@
+package com.peace.exchangerate.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class CrawlingService {
+
+    
+}
