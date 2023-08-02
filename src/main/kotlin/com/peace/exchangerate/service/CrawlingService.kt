b65e48ffdf6e0f0d0559442bceed5becaf070f18
PyeongGangKim <21500185@handong.edu>
2023-08-02 22:50:02 +0900

    feat: naver finance crawling

--- src/main/kotlin/com/peace/exchangerate/service/CrawlingService.kt
+++ src/main/kotlin/com/peace/exchangerate/service/CrawlingService.kt
@@ -1,9 +1,27 @@
 package com.peace.exchangerate.service
 
+import org.jsoup.Jsoup
 import org.springframework.stereotype.Service
 
 @Service
 class CrawlingService {
+    companion object {
+        private const val NAVER_FINANCE_URL = "https://finance.naver.com/marketindex/exchangeList.nhn"
+        private const val COUNTRY_DELIMITER = ".tit"
+        private const val EXCHANGE_RATE_DELIMITER = ".sale"
 
-    
+    }
+
+
+    fun crawling(): MutableMap<String, String> {
+        val scrappedData = Jsoup.connect(NAVER_FINANCE_URL).get()
+        val countries = scrappedData.select(COUNTRY_DELIMITER)
+        val sales = scrappedData.select(EXCHANGE_RATE_DELIMITER)
+        val countryToSale = mutableMapOf<String, String>()
+        countries.forEachIndexed { idx, country ->
+            val sale = sales[idx].toString()
+            countryToSale[country.toString()] = sale
+        }
+        return countryToSale
+    }
 }
