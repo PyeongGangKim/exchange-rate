0b5c779a8b1f31d00ac1eb417b72ebcb81547111
PyeongGangKim <21500185@handong.edu>
2023-08-02 22:50:20 +0900

    feat: crawling RequestMapping

--- src/main/kotlin/com/peace/exchangerate/controller/CrawlingController.kt
+++ src/main/kotlin/com/peace/exchangerate/controller/CrawlingController.kt
@@ -1,11 +1,20 @@
 package com.peace.exchangerate.controller
 
 import com.peace.exchangerate.service.CrawlingService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/crawling")
 class CrawlingController(
     private val crawlingService: CrawlingService,
 ) {
 
+    @GetMapping("/exchange-rate")
+    fun crawlingExchangeRate(): ResponseEntity<Map<String,String>> {
+        val response = crawlingService.crawling()
+        return ResponseEntity.ok(response)
+    }
 }
