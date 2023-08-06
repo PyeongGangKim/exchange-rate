f1c2fb17dd89aa62b2a3cd8af9edcdced2c3113e
PyeongGangKim <21500185@handong.edu>
2023-08-06 22:34:19 +0900

    feat: 관심 있는 환율만 받도록 변경

--- src/main/kotlin/com/peace/exchangerate/service/CrawlingService.kt
+++ src/main/kotlin/com/peace/exchangerate/service/CrawlingService.kt
@@ -1,6 +1,8 @@
 package com.peace.exchangerate.service
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,7 +11,7 @@ class CrawlingService {
         private const val NAVER_FINANCE_URL = "https://finance.naver.com/marketindex/exchangeList.nhn"
         private const val COUNTRY_DELIMITER = ".tit"
         private const val EXCHANGE_RATE_DELIMITER = ".sale"
-
+        private val INTEREST_EXCHANGE_RATE = listOf("미국 USD", "유럽연합 EUR", "일본 JPY (100엔)")
     }
 
 
@@ -18,10 +20,21 @@ class CrawlingService {
         val countries = scrappedData.select(COUNTRY_DELIMITER)
         val sales = scrappedData.select(EXCHANGE_RATE_DELIMITER)
         val countryToSale = mutableMapOf<String, String>()
+        getCountryToSaleInInterestExchangeRate(countries, sales, countryToSale)
+        return countryToSale
+    }
+
+    private fun getCountryToSaleInInterestExchangeRate(
+        countries: Elements,
+        sales: Elements,
+        countryToSale: MutableMap<String, String>
+    ) {
         countries.forEachIndexed { idx, country ->
+            if (isNotInterestExchangeRate(country)) return@forEachIndexed
             val sale = sales[idx].text()
             countryToSale[country.text()] = sale
         }
-        return countryToSale
     }
+
+    private fun isNotInterestExchangeRate(country: Element) = !INTEREST_EXCHANGE_RATE.contains(country.text())
 }
